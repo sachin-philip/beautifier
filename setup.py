f60e1da0b6e6d7ea530c91ca8e59c89ddf643483
Sachin Philip Mathew <sachinvettithanam@gmail.com>
2018-06-20 11:37:25 +0530

    changed the repo url

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from beautifier import __version__
 setup(name='beautifier',
       version=__version__,
       description="Cleanup and beautifier emails, urls and patterns",
-      url='https://github.com/sachinvettithanam/beautifier',
+      url='https://github.com/labtocat/beautifier',
       author='Sachin Philip Mathew',
       author_email='me@imsach.in',
       license='MIT',
       packages=['beautifier'],
       zip_safe=False,
       include_package_data=True
-      )
+    )
